e1d75cb0e59aec2fb060b59c4f88ff82a678ea6e
HearSecurity <hearsectech@gmail.com>
2024-10-07 20:15:54 -0400

    Update proxyflare.py

--- src/proxyflare.py
+++ src/proxyflare.py
@@ -3,10 +3,24 @@
 import httplib
 import sys
 import concurrent.futures
-from colorama import Fore, Back, Style
+from colorama import Fore
 import socket
+from time import sleep 
+
 
 paths = []
+
+def banner(): 
+
+  print """                                                       
+                             __ _                 
+  _ __  _ __ _____  ___   _ / _| | __ _ _ __ ___  
+ | '_ \| '__/ _ \ \/ / | | | |_| |/ _` | '__/ _ \ 
+ | |_) | | | (_) >  <| |_| |  _| | (_| | | |  __/ 
+ | .__/|_|  \___/_/\_\\__, |_| |_|\__,_|_|  \___| 
+ |_|                  |___/                       
+
+  """
 
 def main_function(path):
     
@@ -14,20 +28,20 @@ def main_function(path):
     url = sys.argv[1]
     url = path+"."+url 
     
-    print "Trying: "+url
+    url=url.replace('http://','')
+    url=url.replace('https://','')
+    url=url.replace('www.','')
+
+    print "Trying: "+url 
 
     c = httplib.HTTPSConnection(url)
     c.request("GET", "")
     r = c.getresponse()
-    
-    print r.status
 
     if r.status == 200 or r.status == 301 or r.status == 302:
-
       ip_address = socket.gethostbyname(url)
-
       if "104" not in ip_address:
-        print("[FOUND]: "+url+" IP: "+ip_address)
+        print(Fore.GREEN+"[FOUND]: "+url+" IP: "+ip_address+Fore.WHITE)
         f = open("found.txt", "a")
         f.write(url+" IP: "+ip_address+"\n")
         f.close() 
@@ -44,8 +58,14 @@ def load_domains(domains):
 
 load_domains("subdomains.txt")
 
-print("\n")
-with concurrent.futures.ThreadPoolExecutor(max_workers = 25) as executor:
+if len(sys.argv) < 2: 
+  banner()
+  print "  Usage: python2 proxyflare.py <domain>\n"
+else:
+  banner()
+  print " [*] Wait a few seconds... \n"
+  sleep(2)
+  with concurrent.futures.ThreadPoolExecutor(max_workers = 25) as executor:
     executor.map(main_function, paths)
 
 
